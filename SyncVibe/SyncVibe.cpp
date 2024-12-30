d6d3da2693fb00a77957cfc90b749cf1e4c6e483
Shantzformation <Tsm012@bucknell.edu>
2024-12-30 02:19:41 -0800

    initial

---
+++ SyncVibe/SyncVibe.cpp
@@ -0,0 +1,12 @@
+﻿// SyncVibe.cpp : Defines the entry point for the application.
+//
+
+#include "SyncVibe.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
